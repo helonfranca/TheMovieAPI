470dd0f2759dc5e20123e6a01b383161e3549257
Helon de França <helonfranca02@gmail.com>
2023-05-06 01:18:37 -0300

    implementing method to fetch movies and improving code

--- assets/js/details.js
+++ assets/js/details.js
@@ -3,37 +3,37 @@ const imagePath = 'https://image.tmdb.org/t/p/w500/';
 function getId() {
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get('id');
-    
+
     if (id === null || id === "") {
-        window.location.href = '../index.html'; 
+        window.location.href = '../index.html';
     }
-    
+
     return id;
 }
 
-function showDetailsMovie(id){
-    const httpRequest = new XMLHttpRequest();
-    const url =`https://api.themoviedb.org/3/movie/${id}?api_key=${chave}&language=pt-BR`
+async function showDetailsMovie(id) {
 
-    httpRequest.onreadystatechange = function(){  
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${chave}&language=pt-BR`
+    
+    return fetch(url)
+        .then((response) => response.json())
+        .then((dadosJson) => renderItem(dadosJson));
 
-        if (httpRequest.readyState == 4 && httpRequest.status == 200) {   
-            var dadosjson = JSON.parse(httpRequest.responseText);
-            renderItem(dadosjson);
-        }
-    } 
-
-    httpRequest.open('GET', `${url}`); 
-    httpRequest.send();  
 };
 
-function renderItem(dadosjson){
+function renderItem(dadosjson) {
     var conteiner = document.getElementById("movie");
+    var image = dadosjson.poster_path ? `${imagePath}${dadosjson.poster_path}` : "assets/img/default.jpg";
+    var posterImage = `<img src="${image}">`;
 
-    return conteiner.innerHTML = `<img src="${imagePath}${dadosjson.poster_path}">
+    if (!dadosjson.poster_path) {
+        image = '<img src="caminho_para_imagem_default.jpg">';
+    }
+
+    return conteiner.innerHTML = `${posterImage}
                                     <div class="details">
                                         <h1>${dadosjson.title}</h1>
-                                        <span>Sinopse: ${dadosjson.overview}</span>
+                                        <span>Sinopse: ${dadosjson.overview ? dadosjson.overview : "Sinopse não disponível"} </span>
                                         <span class="release-date">Data de Lançamento: ${dadosjson.release_date}</span>
                                         <a href="../index.html"><button>Voltar</button></a>
                                     </div>`;
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,71 @@
 const imagePath = 'https://image.tmdb.org/t/p/w500/';
 const carregarMais = document.getElementById('carregar');
+const buscaFilme = document.getElementById('submit');
+const inputFilme = document.getElementById('nameMovie');
 let paginaAtual = 1;
 
-function listingMovies(){
-    const url =`https://api.themoviedb.org/3/movie/popular?api_key=${chave}&language=pt-BR&page=${paginaAtual}`
-    const httpRequest = new XMLHttpRequest();
+async function listingMovies() {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${chave}&language=pt-BR&page=${paginaAtual}`
 
-    httpRequest.onreadystatechange = function(){  
+    return fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.results)
+        .then((results) => results.map(renderItems))
 
-        if (httpRequest.readyState == 4 && httpRequest.status == 200) {   
-            
-            var dadosjson = JSON.parse(httpRequest.responseText);
-
-            dadosjson.results.forEach(response => {
-                renderItems(response);
-            });   
-        }
-    } 
-
-    httpRequest.open('GET', `${url}`); 
-    httpRequest.send();  
 };
 
-function renderItems(response){
-    var conteiner =document.getElementById('conteiner');
+async function searchMovies(nameMovie) {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${chave}&language=pt-BR&query=${nameMovie}&page=${paginaAtual}`;
+    var conteiner = document.getElementById('conteiner');
+    conteiner.innerHTML = '';
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.results)
+        .then((results) => results.map(renderItems))
+        .then((movieItens) => {
+            if(movieItens.length === 0){
+                conteiner.innerHTML = '<h3>Não encontramos nenhum filme com esse nome :*(</h3>';
+                carregarMais.style.display = 'none';
+            }else if(movieItens.length > 0){
+                carregarMais.style.display = 'block';
+            }
+        })
+        .catch((erro) => erro)
+}
+
+function renderItems(response) {
+    var conteiner = document.getElementById('conteiner');
+    var image = response.poster_path ? `${imagePath}${response.poster_path}` : "assets/img/default.jpg";
+    var posterImage = `<img src="${image}">`;
+
+    if (!response.poster_path) {
+        image = '<img src="caminho_para_imagem_default.jpg">';
+    }
     return conteiner.innerHTML += `<li>                                           
                                         <a href="../details.html?id=${response.id}">
-                                            <img src="${imagePath}${response.poster_path}" alt="${response.title}">
+                                            ${posterImage}
                                         </a>
                                         <span>${response.title}</span>                                     
                                     </li>`;
 }
 
-carregarMais.addEventListener('click', () => { 
+carregarMais.addEventListener('click', () => {
     paginaAtual++;
-    listingMovies();
+    listingMovies(paginaAtual);
 });
+
+inputFilme.addEventListener("keyup", () => {
+    const nameMovie = inputFilme.value;
+
+    if (nameMovie.length > 0) {
+        setTimeout(() => {
+            searchMovies(nameMovie);
+        }, 1000);
+    } else {
+        listingMovies();
+    }
+})
 
 listingMovies();
 
